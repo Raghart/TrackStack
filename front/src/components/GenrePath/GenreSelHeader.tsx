43cd7e44e64d8e4fc2ac202e398c487d8d8a1630
Rafael Rojas Diaz <118632412+Raghart@users.noreply.github.com>
2025-08-12 15:47:55 -0400

    Fixing mobile visual bugs part 1

--- front/src/components/GenrePath/GenreSelHeader.tsx
+++ front/src/components/GenrePath/GenreSelHeader.tsx
@@ -3,7 +3,7 @@ import { Heading, Highlight } from "@chakra-ui/react";
 const GenreSelHeader = () => {
     return(
         <Heading fontSize={{ base:"4xl", sm: "6xl", md: "75px", lg:"75px"}} userSelect="none" letterSpacing="wider"
-            fontFamily="'Barlow Condensed'" fontWeight="600" lineHeight={1}>
+            fontFamily="'Barlow Condensed'" fontWeight="600" lineHeight={1} textAlign="center">
             <Highlight query="Genre" styles={{ fontSize:{ base: "5xl", sm:"65px", md: "85px", lg:"85px" }, 
             bgGradient: "to-br", bgClip: "text", gradientFrom: "teal.500", gradientTo: "green.500", fontWeight: "800" }}>
                 Select a Genre
--- front/src/components/constants/SongCardC.ts
+++ front/src/components/constants/SongCardC.ts
@@ -1,6 +1,6 @@
 export const SONGCARD_BTN_SIZES = { base: "65px", sm: "75px", md: "80px", lg: "90px" };
 export const SONGCARD_ICON_SIZES = { base: 10, sm: 12, md: 14, lg: 16 };
-export const SONGCARD_FONTSIZES = { base: "17px", sm: "19px", md: "20px", lg: "20px" };
-export const SONGCARD_ART_FONTSIZES = { base: "14px", sm: "15px", md: "15px", lg: "16px" };
+export const SONGCARD_FONTSIZES = { base: "14px", sm: "19px", md: "20px", lg: "20px" };
+export const SONGCARD_ART_FONTSIZES = { base: "12px", sm: "15px", md: "15px", lg: "16px" };
 export const SONGCARD_SIZES = { base: "140px", sm: "165px", md: "180px", lg: "200px" };
 export const LAND_SONGCARD_SIZES = { base: "160px", sm: "180px", md: "200px", lg: "210px" };
