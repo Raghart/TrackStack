ce47de491d0ccc2af1e5b93eeda82fee104403f5
Rafael <rafaelrojas36@outlook.com>
2025-10-01 15:55:06 -0400

    r & t refactoring songGenres module to improve resolver logic

--- back/src/song_genres/song_genres.service.spec.ts
+++ back/src/song_genres/song_genres.service.spec.ts
@@ -35,7 +35,7 @@ describe('SongGenresService', () => {
 
   describe('getAllGenreSongs', () => {
     it('getAllGenreSongs has expected properties', () => {
-      const results = service.getAllGenreSongs(songGenresData);
+      const results = service.parseGenreSongs(songGenresData);
       expect(results.length).toBeGreaterThan(0);
       expectSongProps(results);
     });
@@ -43,29 +43,29 @@ describe('SongGenresService', () => {
     it('getAllGenreSongs throws BadRequestException when seed is not a valid string of numbers', async () => {
       const INVALID_SEED = '18Jenkins18';
       await expect(
-        service.fetchDBSongGenres(INVALID_SEED, 'Rock', 1, 5),
+        service.fetchSongGenres(INVALID_SEED, 'Rock', 1, 5),
       ).rejects.toThrow(BadRequestException);
       await expect(
-        service.fetchDBSongGenres(INVALID_SEED, 'Rock', 1, 5),
+        service.fetchSongGenres(INVALID_SEED, 'Rock', 1, 5),
       ).rejects.toThrow('The seed must be a valid string of numbers.');
     });
 
     it('getAllGenreSongs throws InvalidPaginationException when page < 1', async () => {
-      await expect(
-        service.fetchDBSongGenres('1', 'Rock', 0, 5),
-      ).rejects.toThrow(InvalidPaginationException);
-      await expect(
-        service.fetchDBSongGenres('1', 'Rock', 0, 5),
-      ).rejects.toThrow('Invalid page: 0 must be >= 1');
+      await expect(service.fetchSongGenres('1', 'Rock', 0, 5)).rejects.toThrow(
+        InvalidPaginationException,
+      );
+      await expect(service.fetchSongGenres('1', 'Rock', 0, 5)).rejects.toThrow(
+        'Invalid page: 0 must be >= 1',
+      );
     });
 
     it('getAllGenreSongs throws InvalidPaginationException when limit < 1', async () => {
-      await expect(
-        service.fetchDBSongGenres('1', 'Indie', 1, 0),
-      ).rejects.toThrow(InvalidPaginationException);
-      await expect(
-        service.fetchDBSongGenres('1', 'Indie', 1, 0),
-      ).rejects.toThrow('Invalid limit: 0 must be >= 1');
+      await expect(service.fetchSongGenres('1', 'Indie', 1, 0)).rejects.toThrow(
+        InvalidPaginationException,
+      );
+      await expect(service.fetchSongGenres('1', 'Indie', 1, 0)).rejects.toThrow(
+        'Invalid limit: 0 must be >= 1',
+      );
     });
   });
 });
@@ -89,12 +89,10 @@ describe('SongGenresService Error handler', () => {
 
   describe('getAllGenreSongs Error handler', () => {
     it("getAllGenreSongs throws InternalServerErrorException when it couldn't connect with the DB", async () => {
-      await expect(
-        service.fetchDBSongGenres('1', 'Rock', 1, 5),
-      ).rejects.toThrow(InternalServerErrorException);
-      await expect(
-        service.fetchDBSongGenres('1', 'Rock', 1, 5),
-      ).rejects.toThrow(
+      await expect(service.fetchSongGenres('1', 'Rock', 1, 5)).rejects.toThrow(
+        InternalServerErrorException,
+      );
+      await expect(service.fetchSongGenres('1', 'Rock', 1, 5)).rejects.toThrow(
         'Database Error: SequelizeTimeoutError: Connection refused',
       );
     });
