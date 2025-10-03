60a00a77ca982321252e07c98df2d97c22337fd5
Rafael <rafaelrojas36@outlook.com>
2025-10-02 20:46:10 -0400

    t refactoring artist tests to improve logic and clarification

--- back/src/artists/artists.service.spec.ts
+++ back/src/artists/artists.service.spec.ts
@@ -1,17 +1,18 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArtistsService } from './artists.service';
 import { getModelToken } from '@nestjs/sequelize';
 import {
   BadRequestException,
   InternalServerErrorException,
+  NotFoundException,
 } from '@nestjs/common';
 import {
   artistData,
-  artistSongs,
-} from '../../test/data/artistsModule/serArtistData';
-import { expectSongProps, expectSongsArray } from 'src/utils/expectSongs';
+  artistSongsData,
+  artistTestResponses,
+  artistTestSongs,
+} from '../../test/data/artistsModule/artistData';
+import { expectSongProps } from 'src/utils/expectSongs';
 import {
   SequelizeConnectionError,
   SequelizeTimeoutError,
@@ -21,7 +22,9 @@ import { ArtistsModel } from '../../models/artists/artists.model';
 
 describe('ArtistsService', () => {
   let service: ArtistsService;
-  const LIMIT = artistData.length;
+  let artistModel: { findAll: jest.Mock; findOne: jest.Mock };
+  const artistTestData = artistData.map((entry) => ({ get: () => entry }));
+  const artistSongsDB = { get: () => artistSongsData };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -29,105 +32,121 @@ describe('ArtistsService', () => {
         ArtistsService,
         {
           provide: getModelToken(ArtistsModel),
-          useValue: {},
+          useValue: {
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+          },
         },
       ],
     }).compile();
 
     service = module.get<ArtistsService>(ArtistsService);
+    artistModel = module.get(getModelToken(ArtistsModel));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('fetchArtists returns the expected list of artists from the database', async () => {
+    artistModel.findAll.mockResolvedValue(artistTestData);
+
+    const results = await service.fetchArtists('1');
+    expect(results).toHaveLength(5);
+    expect(results).toStrictEqual(artistData);
   });
 
-  describe('parseArtists', () => {
-    it('parseArtists returns artists with expected props', () => {
-      const results = service.parseArtists(artistData);
-      expect(results.length).toBe(LIMIT);
-
-      results.forEach((artist) =>
-        expect(artist).toMatchObject({
-          id: expect.any(Number),
-          name: expect.any(String),
-          album_cover: expect.any(String),
-        }),
-      );
-    });
-
-    it('fetchDBArtists throws InvalidPaginationException when the page is invalid', async () => {
-      const INVALID_PAGE = -500;
-      await expect(
-        service.fetchArtists('123456', INVALID_PAGE, 5),
-      ).rejects.toThrow(InvalidPaginationException);
-      await expect(
-        service.fetchArtists('123456', INVALID_PAGE, 5),
-      ).rejects.toThrow(`Invalid page: ${INVALID_PAGE} must be >= 1`);
-    });
-
-    it('fetchArtists throws BadRequestException when seed is invalid', async () => {
-      const INVALID_SEED = 'papitas123456';
-      await expect(service.fetchArtists(INVALID_SEED, 1, 5)).rejects.toThrow(
-        BadRequestException,
-      );
-      await expect(service.fetchArtists(INVALID_SEED, 1, 5)).rejects.toThrow(
-        'The seed must be a valid string of numbers.',
-      );
-    });
-
-    it('fetchArtists throws InvalidPaginationException when page = 0', async () => {
-      const INVALID_PAGE = 0;
-      await expect(
-        service.fetchArtists('123456', INVALID_PAGE, 5),
-      ).rejects.toThrow(InvalidPaginationException);
-      await expect(
-        service.fetchArtists('123456', INVALID_PAGE, 5),
-      ).rejects.toThrow(`Invalid page: ${INVALID_PAGE} must be >= 1`);
-    });
-
-    it('fetchArtists throws InvalidPaginationException when the limit is invalid', async () => {
-      const INVALID_LIMIT = -500;
-      await expect(
-        service.fetchArtists('123456', 1, INVALID_LIMIT),
-      ).rejects.toThrow(InvalidPaginationException);
-      await expect(
-        service.fetchArtists('123456', 1, INVALID_LIMIT),
-      ).rejects.toThrow(`Invalid limit: ${INVALID_LIMIT} must be >= 1`);
-    });
-
-    it('fetchArtists throws InvalidPaginationException when the limit = 0', async () => {
-      const INVALID_LIMIT = 0;
-      await expect(
-        service.fetchArtists('123456', 1, INVALID_LIMIT),
-      ).rejects.toThrow(InvalidPaginationException);
-      await expect(
-        service.fetchArtists('123456', 1, INVALID_LIMIT),
-      ).rejects.toThrow(`Invalid limit: ${INVALID_LIMIT} must be >= 1`);
-    });
+  it('parseArtists parses and returns artists with expected props', () => {
+    const results = service.parseArtists(artistData);
+    expect(results).toStrictEqual(artistTestResponses);
   });
 
-  describe('parseArtistSongs', () => {
-    it('parseArtistSongs returns a list of objects with correct properties', () => {
-      const results = service.parseArtistSongs(artistSongs);
-      expectSongProps(results);
-    });
+  it('getAllArtists return a list of artists ready to deliver', async () => {
+    artistModel.findAll.mockResolvedValue(artistTestData);
 
-    it('getAllArtistSongs get specific artist songs', () => {
-      const results = service.parseArtistSongs(artistSongs);
-      const songNames = [
-        'Come as You Are',
-        'Lithium',
-        'Heart-Shaped Box',
-        'In Bloom',
-        'Rape Me',
-      ];
-      expect(results.length).toBeGreaterThan(0);
-      results.forEach((song) => expectSongsArray(song, songNames, 'Nirvana'));
-    });
+    const artistList = await service.getAllArtists('1', 1, 5);
+    expect(artistList).toHaveLength(5);
+    expect(artistList).toStrictEqual(artistTestResponses);
+  });
+
+  it('fetchArtists throws InvalidPaginationException when the page is invalid', async () => {
+    const INVALID_PAGE = -500;
+    await expect(
+      service.fetchArtists('123456', INVALID_PAGE, 5),
+    ).rejects.toThrow(InvalidPaginationException);
+    await expect(
+      service.fetchArtists('123456', INVALID_PAGE, 5),
+    ).rejects.toThrow(`Invalid page: ${INVALID_PAGE} must be >= 1`);
+  });
+
+  it('fetchArtists throws BadRequestException when seed is invalid', async () => {
+    const INVALID_SEED = 'papitas123456';
+    await expect(service.fetchArtists(INVALID_SEED, 1, 5)).rejects.toThrow(
+      BadRequestException,
+    );
+    await expect(service.fetchArtists(INVALID_SEED, 1, 5)).rejects.toThrow(
+      'The seed must be a valid string of numbers.',
+    );
+  });
+
+  it('fetchArtists throws InvalidPaginationException when page = 0', async () => {
+    const INVALID_PAGE = 0;
+    await expect(
+      service.fetchArtists('123456', INVALID_PAGE, 5),
+    ).rejects.toThrow(InvalidPaginationException);
+    await expect(
+      service.fetchArtists('123456', INVALID_PAGE, 5),
+    ).rejects.toThrow(`Invalid page: ${INVALID_PAGE} must be >= 1`);
+  });
+
+  it('fetchArtists throws InvalidPaginationException when the limit is invalid', async () => {
+    const INVALID_LIMIT = -500;
+    await expect(
+      service.fetchArtists('123456', 1, INVALID_LIMIT),
+    ).rejects.toThrow(InvalidPaginationException);
+    await expect(
+      service.fetchArtists('123456', 1, INVALID_LIMIT),
+    ).rejects.toThrow(`Invalid limit: ${INVALID_LIMIT} must be >= 1`);
+  });
+
+  it('fetchArtists throws InvalidPaginationException when the limit = 0', async () => {
+    const INVALID_LIMIT = 0;
+    await expect(
+      service.fetchArtists('123456', 1, INVALID_LIMIT),
+    ).rejects.toThrow(InvalidPaginationException);
+    await expect(
+      service.fetchArtists('123456', 1, INVALID_LIMIT),
+    ).rejects.toThrow(`Invalid limit: ${INVALID_LIMIT} must be >= 1`);
+  });
+
+  it('parseArtistSongs returns a list of objects with correct properties', () => {
+    const results = service.parseArtistSongs(artistSongsData);
+    expectSongProps(results);
+  });
+
+  it('fetchArtistSongs retrieves the artist songs from the database', async () => {
+    artistModel.findOne.mockResolvedValue(artistSongsDB);
+
+    const artistSongs = await service.fetchArtistSongs('Nirvana');
+    expect(artistSongs).toStrictEqual(artistSongsData);
+  });
+
+  it('fetchArtistSongs throws an error if the artist is not found in the database', async () => {
+    await expect(service.fetchArtistSongs('notArtist')).rejects.toThrow(
+      NotFoundException,
+    );
+    await expect(service.fetchArtistSongs('notArtist')).rejects.toThrow(
+      "The artist: 'notArtist' doesn't exist in the DB!",
+    );
+  });
+
+  it('getAllArtistSongs returns a list of songs from the requested artist', async () => {
+    artistModel.findOne.mockResolvedValue(artistSongsDB);
+
+    const artistSongs = await service.getAllArtistSongs('Nirvana');
+    expect(artistSongs).toHaveLength(5);
+    expectSongProps(artistSongs);
+    expect(artistSongs).toStrictEqual(artistTestSongs);
   });
 });
 
-describe('ArtistsService DB Connection Error Handler', () => {
+describe("ArtistsService throws an error when the requests couldn't connect with the database", () => {
   let service: ArtistsService;
 
   beforeEach(async () => {
@@ -147,7 +166,7 @@ describe('ArtistsService DB Connection Error Handler', () => {
     service = module.get<ArtistsService>(ArtistsService);
   });
 
-  it("fetchArtists throws InternalServerErrorException when it couldn't connect with the DB", async () => {
+  it("fetchArtists throws an error when it couldn't connect with the database", async () => {
     await expect(service.fetchArtists('1', 1, 5)).rejects.toThrow(
       InternalServerErrorException,
     );
@@ -156,7 +175,7 @@ describe('ArtistsService DB Connection Error Handler', () => {
     );
   });
 
-  it("fetchArtistSongs throws InternalServerErrorException when it couldn't connect with the DB", async () => {
+  it("fetchArtistSongs throws an error when it couldn't connect with the database", async () => {
     await expect(service.fetchArtistSongs('Felipez')).rejects.toThrow(
       InternalServerErrorException,
     );
--- back/test/data/artistsModule/artistData.ts
+++ back/test/data/artistsModule/artistData.ts
@@ -1,6 +1,9 @@
-export const artistData: any[] = [
+import { ArtistResponse, ArtistWithSongs } from 'src/types/artistAttributes';
+import { SongResponse } from 'src/types/songAttributes';
+
+export const artistData = [
   {
-    id: 598104,
+    id: 1,
     name: 'Nirvana',
     songs: [
       {
@@ -13,7 +16,7 @@ export const artistData: any[] = [
     ],
   },
   {
-    id: 598105,
+    id: 2,
     name: 'Oasis',
     songs: [
       {
@@ -26,7 +29,7 @@ export const artistData: any[] = [
     ],
   },
   {
-    id: 598106,
+    id: 3,
     name: 'Franz Ferdinand',
     songs: [
       {
@@ -39,7 +42,7 @@ export const artistData: any[] = [
     ],
   },
   {
-    id: 598107,
+    id: 4,
     name: 'The Killers',
     songs: [
       {
@@ -52,7 +55,7 @@ export const artistData: any[] = [
     ],
   },
   {
-    id: 598108,
+    id: 5,
     name: 'Radiohead',
     songs: [
       {
@@ -64,9 +67,42 @@ export const artistData: any[] = [
       },
     ],
   },
+] as unknown as ArtistWithSongs[];
+
+export const artistTestResponses: ArtistResponse[] = [
+  {
+    id: 1,
+    name: 'Nirvana',
+    album_cover:
+      'https://i.scdn.co/image/ab67616d0000b27385e5dcc05cc216a10f141480',
+  },
+  {
+    id: 2,
+    name: 'Oasis',
+    album_cover:
+      'https://i.scdn.co/image/ab67616d0000b27385e5dcc05cc216a10f141480',
+  },
+  {
+    id: 3,
+    name: 'Franz Ferdinand',
+    album_cover:
+      'https://i.scdn.co/image/ab67616d0000b27385e5dcc05cc216a10f141480',
+  },
+  {
+    id: 4,
+    name: 'The Killers',
+    album_cover:
+      'https://i.scdn.co/image/ab67616d0000b27385e5dcc05cc216a10f141480',
+  },
+  {
+    id: 5,
+    name: 'Radiohead',
+    album_cover:
+      'https://i.scdn.co/image/ab67616d0000b27385e5dcc05cc216a10f141480',
+  },
 ];
 
-export const artistSongs: any = {
+export const artistSongsData = {
   id: 598104,
   name: 'Nirvana',
   songs: [
@@ -121,4 +157,52 @@ export const artistSongs: any = {
       },
     },
   ],
-};
+} as unknown as ArtistWithSongs;
+
+export const artistTestSongs: SongResponse[] = [
+  {
+    id: 598102,
+    name: 'Come as You Are',
+    artists: ['Nirvana'],
+    url_preview:
+      'https://p.scdn.co/mp3-preview/a1c11bb1cb231031eb20e5951a8bfb30503224e9?cid=774b29d4f13844c495f206cafdad9c86',
+    album_cover:
+      'https://i.scdn.co/image/ab67616d0000b27385e5dcc05cc216a10f141480',
+  },
+  {
+    id: 584103,
+    name: 'Lithium',
+    artists: ['Nirvana'],
+    url_preview:
+      'https://p.scdn.co/mp3-preview/a1c11bb1cb231031eb20e5951a8bfb30503224e9?cid=774b29d4f13844c495f206cafdad9c86',
+    album_cover:
+      'https://i.scdn.co/image/ab67616d0000b27385e5dcc05cc216a10f141480',
+  },
+  {
+    id: 584321,
+    name: 'Heart-Shaped Box',
+    artists: ['Nirvana'],
+    url_preview:
+      'https://p.scdn.co/mp3-preview/a1c11bb1cb231031eb20e5951a8bfb30503224e9?cid=774b29d4f13844c495f206cafdad9c86',
+    album_cover:
+      'https://i.scdn.co/image/ab67616d0000b27385e5dcc05cc216a10f141480',
+  },
+  {
+    id: 598123,
+    name: 'In Bloom',
+    artists: ['Nirvana'],
+    url_preview:
+      'https://p.scdn.co/mp3-preview/a1c11bb1cb231031eb20e5951a8bfb30503224e9?cid=774b29d4f13844c495f206cafdad9c86',
+    album_cover:
+      'https://i.scdn.co/image/ab67616d0000b27385e5dcc05cc216a10f141480',
+  },
+  {
+    id: 597132,
+    name: 'Rape Me',
+    artists: ['Nirvana'],
+    url_preview:
+      'https://p.scdn.co/mp3-preview/a1c11bb1cb231031eb20e5951a8bfb30503224e9?cid=774b29d4f13844c495f206cafdad9c86',
+    album_cover:
+      'https://i.scdn.co/image/ab67616d0000b27385e5dcc05cc216a10f141480',
+  },
+];
