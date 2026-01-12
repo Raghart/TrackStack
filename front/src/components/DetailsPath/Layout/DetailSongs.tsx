8f43ad605b292234cc4ef1e61eef728726e0a3d6
Raghart <rafaelrojas36@outlook.com>
2026-01-12 16:58:10 -0400

    r refactoring DetailSongs infinite-Scroll

--- front/src/components/DetailsPath/Layout/DetailSongs.tsx
+++ front/src/components/DetailsPath/Layout/DetailSongs.tsx
@@ -1,5 +1,4 @@
-import { SimpleGrid, Skeleton } from "@chakra-ui/react";
-import InfiniteScrollList from "@/components/Pagination/InfiniteScrollList";
+import { Center, For, SimpleGrid, Skeleton } from "@chakra-ui/react";
 import SongCard from "../../SongCard/SongCard";
 import { ValidDetail } from "@/types/utilTypes";
 import NotFound from "@/components/Error/NotFound";
@@ -7,23 +6,34 @@ import { useAppSelector } from "@/components/Utils/redux-hooks";
 import useGetDetailSongs from "@/components/Utils/hooks/useGetDetailSongs";
 import { SONGCARD_SIZES } from "@/components/constants/SongCardC";
 import { SongResponse } from "@/types/songTypes";
+import InfiniteScroll from "react-infinite-scroll-component";
+import PaginateSongs from "@/components/Pagination/PaginateSongs";
+import LoadingBeat from "@/components/Utils/LoadingBeat";
 
 const DetailSongs = ({ type, filterValue, error }: { type: ValidDetail, filterValue: string, error: string }) => {
-    const { data, loading, visibleSongs, onLoadMore } = useGetDetailSongs(type, filterValue);
+    const { data, loading, detailSongs } = useGetDetailSongs(type, filterValue);
+    const { visibleSongs, loadMoreSongs } = PaginateSongs(detailSongs);
+
     const { activeSong, isPlaying } = useAppSelector(state => state.songs.songState);
-    if (!loading && !data && visibleSongs.length === 0) return <NotFound message={error} />
+    if (!loading && !data && detailSongs.length === 0) return <NotFound message={error} />
 
     return(
-        <SimpleGrid columns={{ base: 2, sm: 3, md: 4, lg: 5 }} gap={{ base: 3, sm: 3, md: 3, lg: 4 }} w="full">
-            {data ? (
-                <InfiniteScrollList renderItem={(song: SongResponse) => <SongCard key={song.id} {...song} 
-                    isSongPlaying={isPlaying && activeSong?.id === song.id} />} onLoadMore={onLoadMore}
-                    error={`There was an error while Trying to manage the received data!`} items={visibleSongs} />
-            ) : (
-                Array.from({ length: 20 }).map((_, idx) => (
-                <Skeleton key={idx} w="full" maxW={SONGCARD_SIZES} aspectRatio={3/4} borderRadius="md" />
-            )))}   
-        </SimpleGrid>
+        <InfiniteScroll dataLength={visibleSongs.length} hasMore={visibleSongs.length < detailSongs.length} 
+            loader={<Center m={2}><LoadingBeat /></Center>} next={loadMoreSongs} 
+            style={{ display: "block", paddingBottom: 80 }}>
+            <SimpleGrid columns={{ base: 2, sm: 3, md: 4, lg: 5 }} gap={{ base: 3, sm: 3, md: 3, lg: 4 }} 
+                w="full">
+                {!data && (Array.from({ length: 20 }).map((_, idx) => <Skeleton key={idx} w="full" 
+                    maxW={SONGCARD_SIZES} loading={loading} aspectRatio={3/4} borderRadius="md" />))}
+                
+                {data && (
+                    <For each={visibleSongs}>
+                        {(song: SongResponse) => <SongCard key={song.id} {...song} 
+                            isSongPlaying={isPlaying && activeSong?.id === song.id} />}
+                    </For>
+                )}   
+            </SimpleGrid>
+        </InfiniteScroll>
     );
 };
 
---
+++ front/src/components/Pagination/PaginateSongs.ts
@@ -0,0 +1,16 @@
+import { SongResponse } from "@/types/songTypes";
+import { useState } from "react";
+
+const PaginateSongs = (songs: SongResponse[]) => {
+    const [visibleCount, setVisibleCount] = useState<number>(20);
+    const visibleSongs = songs.slice(0, visibleCount);
+    const loadMoreSongs = () => {
+        if (visibleSongs.length < songs.length) {
+            setVisibleCount(prev => prev + 20);
+        } 
+    }
+
+    return { visibleSongs, loadMoreSongs }
+};
+
+export default PaginateSongs;
