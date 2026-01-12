fc10694fd8ee3ff8827365ec4837cee9eacba7b0
Raghart <rafaelrojas36@outlook.com>
2026-01-12 10:33:44 -0400

    r refactoring ArtistSelection to include InfiniteScrolling

--- front/src/components/ArtistPath/ArtistSelection.tsx
+++ front/src/components/ArtistPath/ArtistSelection.tsx
@@ -1,35 +1,42 @@
-import InfiniteScrollList from "@/components/Pagination/InfiniteScrollList";
-import { Flex, SimpleGrid, Skeleton } from "@chakra-ui/react";
+import { Box, Center, For, SimpleGrid, Skeleton } from "@chakra-ui/react";
 import ScrollUpArrow from "@/components/Utils/ScrollUpArrow";
-import ArtistCard from "./ArtistCard";
 import ArtistHeader from "./ArtistHeader";
 import useGetArtistCards from "../Utils/hooks/useGetArtistCards";
 import { Zoom } from "react-awesome-reveal";
 import { ARTIST_CARD_SIZES } from "../constants/ArtistPathC";
+import InfiniteScroll from "react-infinite-scroll-component"
 import { ArtistResponse } from "@/types/artistTypes";
+import ArtistCard from "./ArtistCard";
+import LoadingBeat from "../Utils/LoadingBeat";
 
 const ArtistSelection = () => {
     const { data, onLoadMore } = useGetArtistCards();
-    return(
-        <Flex w="full" h="full" direction="column" justify="center" align="center" pt={8} gap={12} pb={20}>
-            <Zoom triggerOnce direction="down" delay={100}>
-                <ArtistHeader />
-            </Zoom>
+    var dataLength = data?.getAllArtists ? data.getAllArtists.length : 20;
 
-            <SimpleGrid columns={{ base: 2, sm: 3, md: 4, lg: 5 }} w="full" gap={4}>
-                {!data && (Array.from({ length: 20 }).map((_, idx) => (
-                    <Skeleton key={idx} w="full" maxW={ARTIST_CARD_SIZES} aspectRatio={3/4} borderRadius="2xl" />
-                )))}
-                
-                {data && (
-                    <InfiniteScrollList<ArtistResponse> items={data.getAllArtists} onLoadMore={onLoadMore}
-                        error="Error Trying to get the artists" renderItem={(artist: ArtistResponse) => (
-                            <ArtistCard key={artist.name} {...artist} />
-                    )} />
-                )}
-            </SimpleGrid>
+    return(
+        <Box w="full" h="full" direction="column" pt={8} gap={12}>
+            <Zoom triggerOnce direction="down" delay={100} style={{ paddingBottom: 40 }}>
+                <ArtistHeader />
+            </Zoom>    
+
+            <InfiniteScroll dataLength={dataLength} next={onLoadMore} hasMore={true}
+                loader={<Center m={2}><LoadingBeat /></Center>} style={{ overflow: "visible" }}>
+                <SimpleGrid columns={{ base: 2, sm: 3, md: 4, lg: 5 }} w="full" gap={4}>
+                    {!data && (Array.from({ length: 20 }).map((_, idx) => (
+                        <Skeleton key={idx} w="full" maxW={ARTIST_CARD_SIZES} aspectRatio={3/4} 
+                            borderRadius="2xl" inset={0} />
+                        ))
+                    )}
+
+                    {data && (
+                        <For each={data.getAllArtists}>
+                            {(artist: ArtistResponse) => <ArtistCard key={artist.name} {...artist} />}
+                        </For>
+                    )}
+                </SimpleGrid>
+            </InfiniteScroll>
             <ScrollUpArrow />
-        </Flex>
+        </Box>
     );
 };
 
