640df38298c79d5496bbea84a5320b2a4408c0e3
Raghart <rafaelrojas36@outlook.com>
2026-01-12 20:17:43 -0400

    r refactoring LaraRecommendations to use new inf-Scroll

--- front/src/components/LaraRecommendationsPath/LaraRecommendations.tsx
+++ front/src/components/LaraRecommendationsPath/LaraRecommendations.tsx
@@ -1,5 +1,4 @@
-import { Flex, SimpleGrid } from "@chakra-ui/react";
-import InfiniteScrollList from "../Pagination/InfiniteScrollList";
+import { Box, Center, For, SimpleGrid } from "@chakra-ui/react";
 import SongCard from "../SongCard/SongCard";
 import LaraHeader from "./LaraHeader";
 import { Navigate } from "react-router-dom";
@@ -8,28 +7,35 @@ import useSongRec from "../Utils/hooks/useSongRec";
 import { useAppSelector } from "../Utils/redux-hooks";
 import { Zoom } from "react-awesome-reveal";
 import { SongResponse } from "@/types/songTypes";
+import InfiniteScroll from "react-infinite-scroll-component";
+import LoadingBeat from "../Utils/LoadingBeat";
 
 const LaraRecommendations = () => {
-    const { visibleSongs, loadMoreSongs } = useSongRec();
+    const { visibleSongs, recommendations, loadMoreSongs } = useSongRec();
     const { activeSong, isPlaying } = useAppSelector(state => state.songs.songState);
     if (visibleSongs.length === 0) return <Navigate to="/" replace />
+    
     return(
-        <Flex direction="column" align="center" justify="center" pt={7} pb={14} gap={7}>
-            <Zoom triggerOnce direction="down" delay={100}>
+        <Box direction="column" pt={7} pb={24} gap={7}>
+            <Zoom triggerOnce direction="down" delay={100} style={{ paddingBottom: 20 }}>
                 <LaraHeader />
             </Zoom>
 
             <Zoom triggerOnce direction="up" delay={100}>
-                <SimpleGrid columns={{ base: 2, sm: 3, md: 4, lg: 5 }} pt={4} gap={{ base: 3, sm: 3, md: 3, lg: 4 }} 
-                    w="full">
-                    <InfiniteScrollList renderItem={(song: SongResponse) => <SongCard key={song.id} {...song} 
-                        isSongPlaying={isPlaying && activeSong?.id === song.id} />} items={visibleSongs}
-                        error="There are no song Recommendations avaible in the DB with the data provided"  
-                        onLoadMore={loadMoreSongs} />
-                </SimpleGrid>
+                <InfiniteScroll hasMore={visibleSongs.length < recommendations.length} next={loadMoreSongs}
+                    dataLength={visibleSongs.length} loader={<Center m={2}><LoadingBeat /></Center>}
+                    style={{ overflow: "visible" }}>
+                    <SimpleGrid columns={{ base: 2, sm: 3, md: 4, lg: 5 }} pt={4} w="full" 
+                        gap={{ base: 3, sm: 3, md: 3, lg: 4 }}>
+                        <For each={visibleSongs}>
+                            {(song: SongResponse) => <SongCard key={song.id} {...song} 
+                                isSongPlaying={isPlaying && activeSong?.id === song.id} />}
+                        </For>
+                    </SimpleGrid>
+                </InfiniteScroll>
             </Zoom>
             <ScrollUpArrow />
-        </Flex>
+        </Box>
     );
 };
 
