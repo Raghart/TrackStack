74daa212e5e0a6236a989d6bcdb5b5b9cc1f303e
Raghart <rafaelrojas36@outlook.com>
2026-02-27 18:43:33 -0400

    r upgrading generateLoudness

--- front/src/components/Utils/generateLoudness.ts
+++ front/src/components/Utils/generateLoudness.ts
@@ -1,17 +1,26 @@
+import { loudnessMAX } from "../constants/ModalC";
 import randInRange from "./randInRange";
 
 const generateLoudness = (mood: number, sentiment: number) : number => {
     switch (true) {
+        case sentiment > 0.9 && mood === 1:
+            return randInRange(-3, loudnessMAX);
         case sentiment > 0.8 && mood === 1:
-            return randInRange(-3, 0);
-        case sentiment > 0.6 && mood === 1:
             return randInRange(-6, -3);
+        case sentiment > 0.7 && mood === 1:
+            return randInRange(-10, -6);
+        case sentiment > 0.6 && mood === 1:
+            return randInRange(-16, -10);
         case mood === 1:
-            return randInRange(-10,-3);
+            return randInRange(-22,-16);
         case sentiment > 0.4 && mood == 0:
-            return randInRange(-20, -10)
+            return randInRange(-30, -22);
+        case sentiment > 0.3 && mood === 0:
+            return randInRange(-40, -30);
+        case sentiment > 0.2 && mood === 0:
+            return randInRange(-50, -40);
         default:
-            return randInRange(-50, -21);
+            return randInRange(-60, -50);
     }
 };
 
