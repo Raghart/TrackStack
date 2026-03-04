2cab53ee4e1e532418db78477979bc06ba50d05c
Raghart <rafaelrojas36@outlook.com>
2026-03-04 11:04:47 -0400

    r fixed album ApolloClient cache to recieve more data

--- front/src/apolloClient.ts
+++ front/src/apolloClient.ts
@@ -32,6 +32,19 @@ export const client = new ApolloClient({
               return merged;
             }
           },
+          getAlbums: {
+            keyArgs: false,
+            merge(existing, incoming, {args}) {
+              const page = args?.page ?? 1;
+              const limit = args?.limit ?? 20;
+              const offset = (page - 1) * limit;
+              const merged = existing ? existing.slice(0) : [];
+              for (let i=0; i < incoming.length; i++) {
+                merged[offset + i] = incoming[i]
+              }
+              return merged;
+            }
+          }
         }
       }
     }
--- front/src/components/Utils/hooks/useGetAlbumCards.ts
+++ front/src/components/Utils/hooks/useGetAlbumCards.ts
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 const useGetAlbumCards = () => {
     const [page, setPage] = useState<number>(1);
-    const [seed] = useState(Math.random().toString().slice());
+    const [seed] = useState(Math.random().toString().slice(2));
     const { data, fetchMore } = useQuery(getAlbums, {
         variables: { seed, page: 1, limit: 20 },
         notifyOnNetworkStatusChange: true,
