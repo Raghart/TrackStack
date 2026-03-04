5f7ecd0e1f276217c7445e55b3dd98788077b32a
Raghart <rafaelrojas36@outlook.com>
2026-03-04 16:54:28 -0400

    t test parseGenre for enhancing pop genres

--- back/src/utils/parseGenres.spec.ts
+++ back/src/utils/parseGenres.spec.ts
@@ -1,10 +1,15 @@
-import { rockGenreArr } from "./genreConstantsArrays";
+import { popGenreArr, rockGenreArr } from "./genreConstantsArrays";
 import parseGenres from "./parseGenres";
 
 describe('Parse genres from userVector to enhance response', () => {
     it("enchances rock with more options when rock is searched", () => {
         const parsedResults = parseGenres(["Rock"]);
         expect(parsedResults).toStrictEqual(rockGenreArr);
+    });
+
+    it("enchances pop genre with more options when Pop is searched", () => {
+        const parsedResults = parseGenres(["Pop"]);
+        expect(parsedResults).toStrictEqual(popGenreArr);
     });
 
     it("returns the same array if no genre matches the enhanced option", () => {
