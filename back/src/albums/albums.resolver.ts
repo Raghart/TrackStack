6102c5cc21bf6a2ff4b549c681cde345fb7a0652
Raghart <rafaelrojas36@outlook.com>
2026-03-03 20:30:09 -0400

    r implemmented albumResultsDto and resolver func

--- back/src/albums/albums.resolver.ts
+++ back/src/albums/albums.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { AlbumsService } from './albums.service';
 import { GraphQLString } from 'graphql';
 import { SongResponseDto } from 'src/songs/dto/SongResponse.dto';
@@ -6,6 +6,15 @@ import { SongResponseDto } from 'src/songs/dto/SongResponse.dto';
 @Resolver()
 export class AlbumsResolver {
   constructor(private readonly albumsService: AlbumsService) {}
+
+  @Query(() => Text, { name: 'getAlbums' })
+  async getAlbums(
+    @Args('seed', { type: () => GraphQLString }) seed: string,
+    @Args('page', { type: () => Int, defaultValue: 1 }) page: number,
+    @Args('limit', { type: () => Int, defaultValue: 20 }) limit: number,
+  ) : Promise<string> {
+    return this.albumsService.fetchAlbums(seed, page, limit)
+  }
 
   @Query(() => [SongResponseDto], { name: 'getAllAlbumSongs' })
   async getAllAlbumSongs(
---
+++ back/src/albums/dto/AlbumResultsDto.ts
@@ -0,0 +1,16 @@
+import { Field, ObjectType } from "@nestjs/graphql";
+
+@ObjectType()
+export class AlbumsResultsDto {
+    @Field()
+    id: number;
+
+    @Field()
+    name: string;
+
+    @Field()
+    url_image: string;
+
+    @Field()
+    artists: string[];
+}
