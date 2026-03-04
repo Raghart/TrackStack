61ac5ccfa0e2696bc350b280aba02925c863ea0e
Raghart <rafaelrojas36@outlook.com>
2026-03-04 16:47:07 -0400

    t added test for parsing Rock Genres

--- back/src/utils/parseGenres.spec.ts
+++ back/src/utils/parseGenres.spec.ts
@@ -3,7 +3,7 @@ import parseGenres from "./parseGenres";
 
 describe('Parse genres from userVector to enhance response', () => {
     it("enchances rock with more options when rock is searched", () => {
-        const parsedResults = parseGenres(["rock"]);
-        expect(parsedResults).toContain(rockGenreArr);
+        const parsedResults = parseGenres(["Rock"]);
+        expect(parsedResults).toStrictEqual(rockGenreArr);
     });
 });
