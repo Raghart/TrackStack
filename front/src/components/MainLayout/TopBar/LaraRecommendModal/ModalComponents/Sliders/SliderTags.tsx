4cf40ca0adc88f08f330b939c1fccaacd7df7de0
Raghart <rafaelrojas36@outlook.com>
2026-03-19 18:35:02 -0400

    r added MapSliderStyle for security check

--- front/src/components/MainLayout/TopBar/LaraRecommendModal/ModalComponents/Sliders/SliderTags.tsx
+++ front/src/components/MainLayout/TopBar/LaraRecommendModal/ModalComponents/Sliders/SliderTags.tsx
@@ -1,5 +1,5 @@
 import { SLIDER_TAGS_GAP_SIZES, SLIDER_TAGS_PX_SIZES } from "@/components/constants/TopBarC";
-import { TagSliderStyles } from "@/dynamicCSS/TagsSliderStyles";
+import { MapSliderStyles, TagSliderStyles } from "@/dynamicCSS/TagsSliderStyles";
 import { SliderLabels } from "@/types/RecDataTypes";
 import { Flex, For, Tag } from "@chakra-ui/react";
 
@@ -7,13 +7,23 @@ const SliderTags = ({ labels } : { labels: SliderLabels[] }) => {
     return(
         <Flex mt={3} gap={SLIDER_TAGS_GAP_SIZES}>
             <For each={labels}>
-                {(label) => ( 
+                {(label) => {
+                    let labelStyle = MapSliderStyles.get(label);
+                    if (labelStyle === undefined) {
+                        labelStyle = {
+                            bg: "gray.500", 
+                            border: "0 0 0 2px var(--chakra-colors-gray-500), 0 0 8px var(--chakra-colors-gray-500)"
+                        }
+                    };
+                    return ( 
                     <Tag.Root key={label} size="md" variant="subtle" borderRadius="full" color="gray.300" 
-                        bg={TagSliderStyles[label].bg} py={1} fontWeight="800" fontSize="'Barlow', sans-serif"
-                        boxShadow={TagSliderStyles[label].border} letterSpacing="wide" px={SLIDER_TAGS_PX_SIZES}>
+                        bg={labelStyle.bg} py={1} fontWeight="800"
+                        boxShadow={labelStyle.border} letterSpacing="wide" 
+                        px={SLIDER_TAGS_PX_SIZES} fontSize="'Barlow', sans-serif">
                             <Tag.Label>{label}</Tag.Label>
                     </Tag.Root>
-                )}
+                )
+                }}
             </For>
         </Flex>
     );
