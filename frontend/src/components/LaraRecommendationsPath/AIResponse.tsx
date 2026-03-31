ada90b439481327e016e7e759dfb712959f73381
Raghart <rafaelrojas36@outlook.com>
2026-03-30 20:55:45 -0400

    r improving AIResponse component code structure

--- frontend/src/components/LaraRecommendationsPath/AIResponse.tsx
+++ frontend/src/components/LaraRecommendationsPath/AIResponse.tsx
@@ -1,18 +1,16 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Center } from "@chakra-ui/react"
 import LoadingBeat from "../Utils/LoadingBeat"
 import Markdown from "react-markdown"
 
 const AIResponse = ({ message } : { message: string }) => {
     return(
         <Box color="white" userSelect="none" fontFamily="'Barlow', sans-serif" fontWeight="medium" 
-            fontSize={{ base: "md", sm: "lg", md: "xl", lg: "xl" }} fontStyle="italic">
-            {!message && (
-                <LoadingBeat />
-            )}
-
-            {message && (
-                <Markdown skipHtml>{message}</Markdown>
-            )}
+            fontSize={{ base: "md", sm: "lg", md: "xl", lg: "xl" }} fontStyle="italic" alignItems="center">
+            {message ? (<Markdown skipHtml>{message}</Markdown>) : (
+                <Center>
+                    <LoadingBeat />
+                </Center>
+                )}
         </Box>
     );
 };
