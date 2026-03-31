f680e1a325ad994bfdca7f6c93b5bd9077574a77
Raghart <rafaelrojas36@outlook.com>
2026-03-31 18:25:02 -0400

    r moved & updated build_steps to ease deploying

--- build_steps.sh
+++ build_steps.sh
@@ -1,16 +1,10 @@
 echo "Build TrackStack script"
 
-cd ..
-
 echo "Installing Backend and Frontend dependencies"
 npm install
 
-cd backend/
+echo "Build Frontend"
+npm run build:frontend
 
 echo "Build Backend"
-npm run build
-
-cd ../frontend
-
-echo "Build Frontend"
-npm run build
+npm run build:backend
