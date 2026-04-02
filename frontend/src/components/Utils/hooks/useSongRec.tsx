de72012cbdfd15cbc3a02d074961b70be7effd7c
Raghart <rafaelrojas36@outlook.com>
2026-04-01 21:37:48 -0400

    r added timer to test if it works in prod

--- frontend/src/components/Utils/hooks/useSongRec.tsx
+++ frontend/src/components/Utils/hooks/useSongRec.tsx
@@ -8,6 +8,7 @@ const useSongRec = () => {
     const hasFetched = useRef(false);
     const [visibleCount, setVisibleCount] = useState<number>(20);
     const [aiResponse, setAIResponse] = useState<string>("");
+    const [isSub, setIsSub] = useState(false);
     const [streamAnswer] = useMutation(streamAIAnswer);
     const { genres, energy, speechLevel, danceability, tempo, sentiment, voiceType, 
             mood, acousticness } = useAppSelector(state => state.songData);
@@ -25,12 +26,17 @@ const useSongRec = () => {
     });
 
     useEffect(() => {
-        if (hasFetched.current || !loading) return;
+        const timer = setTimeout(() => setIsSub(false), 500);
+        return () => clearTimeout(timer)
+    }, [])
+
+    useEffect(() => {
+        if (hasFetched.current || !loading || !isSub) return;
         hasFetched.current = true;
         setAIResponse("");
         console.log("using stream answer!")
         streamAnswer({ variables: { genres, userVector } });
-    }, [genres, userVector, streamAnswer, loading]);
+    }, [genres, userVector, streamAnswer, loading, isSub]);
 
     const loadMoreSongs = () => {
         if (visibleSongs.length < recommendations.length) {
